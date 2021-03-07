2dbda1fc4c3e506f8a832bea0adcec851700c7a2
Jérémie <jeremielegrand021@gmail.com>
2021-03-07 14:53:51 +0100

    [CRUD] make crud of all entity

---
+++ src/Controller/Admin/FixController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Fix;
+use App\Form\FixType;
+use App\Repository\FixRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/fix")
+ */
+class FixController extends AbstractController
+{
+    /**
+     * @Route("/", name="fix_index", methods={"GET"})
+     */
+    public function index(FixRepository $fixRepository): Response
+    {
+        return $this->render('fix/index.html.twig', [
+            'fixes' => $fixRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="fix_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $fix = new Fix();
+        $form = $this->createForm(FixType::class, $fix);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($fix);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('fix_index');
+        }
+
+        return $this->render('fix/new.html.twig', [
+            'fix' => $fix,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{idfix}", name="fix_show", methods={"GET"})
+     */
+    public function show(Fix $fix): Response
+    {
+        return $this->render('fix/show.html.twig', [
+            'fix' => $fix,
+        ]);
+    }
+
+    /**
+     * @Route("/{idfix}/edit", name="fix_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Fix $fix): Response
+    {
+        $form = $this->createForm(FixType::class, $fix);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('fix_index');
+        }
+
+        return $this->render('fix/edit.html.twig', [
+            'fix' => $fix,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{idfix}", name="fix_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Fix $fix): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$fix->getIdfix(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($fix);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('fix_index');
+    }
+}
